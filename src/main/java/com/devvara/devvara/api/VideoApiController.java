1e1e7ecfb52f83379e88317f6bcb2ff09311a74f
Yoochan Hwang <yoochan1868@gmail.com>
2023-05-15 21:46:22 +0900

    Converted entity to DTO

--- src/main/java/com/devvara/devvara/api/VideoApiController.java
+++ src/main/java/com/devvara/devvara/api/VideoApiController.java
@@ -2,13 +2,17 @@ package com.devvara.devvara.api;
 
 import com.devvara.devvara.domain.Video;
 import com.devvara.devvara.service.VideoService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,12 +24,33 @@ public class VideoApiController {
     private final VideoService videoService;
 
     @GetMapping("/api/v1/videos")
-    public Page<Video> videosV2(@RequestParam(required = false) String[] language,
+    public List<VideoListDto> videosV2(@RequestParam(required = false) String[] language,
                                 @RequestParam(required = false) String title,
                                 @RequestParam(required = false) Long channelId,
                                 @RequestParam(defaultValue = "1") int page,
                                 @RequestParam(defaultValue = "12") int size) {
-        return videoService.findVideos(language, title, channelId, page, size);
+
+        Page<Video> pageData = videoService.findVideos(language, title, channelId, page, size);
+        List<Video> content = pageData.getContent();
+        int pageNumber = pageData.getNumber();
+        int totalPages = pageData.getTotalPages();
+        List<VideoListDto> result = new ArrayList<>();
+        result.add(new VideoListDto(content, pageNumber, totalPages));
+
+        return result;
+    }
+
+    @Data
+    static class VideoListDto {
+        private List<Video> content;
+        private int pageNumber;
+        private int totalPage;
+
+        public VideoListDto(List<Video> content, int pageNumber, int totalPages) {
+            this.content = content;
+            this.pageNumber = pageNumber;
+            this.totalPage = totalPages;
+        }
     }
 
 }
