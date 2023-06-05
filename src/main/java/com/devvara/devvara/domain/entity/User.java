3d8a6638f5e03f10a16c79fb8610660df4660802
Yoochan Hwang <yoochan1868@gmail.com>
2023-06-05 21:55:38 +0900

    Updated user entity

--- src/main/java/com/devvara/devvara/domain/entity/User.java
+++ src/main/java/com/devvara/devvara/domain/entity/User.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
 
     @Id
@@ -15,10 +17,22 @@ public class User {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
+    @Column(name = "email_verified_at")
+    private LocalDateTime emailVerifiedAt;
 
     @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "remember_token")
+    private String rememberToken;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
